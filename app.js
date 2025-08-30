a96a3dba094bf41753b6890ebdbc26e8643c0d49
Gulam Mustafa <gulammustafa64m@gmail.com>
2025-08-30 08:38:28 -0700

    Second Commit

--- app.js
+++ app.js
@@ -11,5 +11,26 @@ function delone(){
     display.value = display.value.slice(0,-1);
 }
 function lastequal(){
-    display.value = eval(display.value);
+    let exponent = display.value;
+let operator = "-+*/%";
+let error = false;
+
+ 
+for(let i = 0; i < exponent.length -1; i++){
+    var current = exponent[i];
+    var  next = exponent[i + 1];
+
+if(operator.indexOf(current) !== -1 && operator.indexOf(next) !== -1){
+
+        error = true;
+        break;
+    }
+
+}
+
+if (error){
+    display.value = "Error";
+}else {
+    display.value = eval(exponent);
+}
 }
